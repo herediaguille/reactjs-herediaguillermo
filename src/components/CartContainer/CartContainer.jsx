9a0e2ef47d28200b29fa0335e762398d6d5a084a
Guille Heredia <heredia.guille@gmail.com>
2023-11-28 00:00:05 -0300

    finalizar compra

--- src/components/CartContainer/CartContainer.jsx
+++ src/components/CartContainer/CartContainer.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "react"
+import { useContext, useState } from "react"
 import { CartContext } from "../../CartContext/CartContext"
 import { addDoc, collection, getFirestore } from "firebase/firestore";
 
@@ -9,15 +9,24 @@ import ListGroup from 'react-bootstrap/ListGroup';
 
 export const CartContainer = () => {
 
-    const {cartList, emptyCart, removeProduct} = useContext(CartContext)
+    const [ isId, setIsId ] = useState('')
+    const [ formData, setformData ] = useState({
+        name: '',
+        phone: '',
+        email: '',
+        adress: ''
+    })
+
+    const { cartList, emptyCart, removeProduct } = useContext(CartContext)
 
     const totalPrice = () => {
         return cartList.reduce((total, product) => total + product.price * product.cant, 0);
     }
 
-    const getOrder = () => {
+    const getOrder = (evt) => {
+        evt.preventDefault()
         const order = {}
-        order.buyer = {nombre: 'guille', phone: '3513874444', email: 'heredia.guille@gmail.com'}
+        order.buyer = formData
         order.items = cartList.map( product => ( { id: product.id, price: product.price, title: product.title, cant: product.cant} ) )
         order.total = totalPrice()        
         
@@ -25,41 +34,75 @@ export const CartContainer = () => {
         const queryCollection = collection (db, 'orders')
 
         addDoc(queryCollection, order)
-        .then (res => console.log(res))
+        .then (({id}) => console.log(id))
         .catch (error => console.log(error))
+        .finally (()=> {
+            setformData({
+                name: '',
+                phone: '',
+                email: '',
+                adress: ''
+            })
+            emptyCart()
+        })
+    }
+    const handleOnChange = (evt) => {
+        setformData({
+            ...formData,
+            [evt.target.name]: evt.target.value
+        })
     }
 
     return (
-        <div>
-            <div className="container"> 
-            {cartList.map(product => 
-            <div className="row">
-            <div className="col-lg-3 col-md-6" >
-                <Card>
-                    <Card.Img variant="top" src={product.img} alt={product.title} className="rounded d-block"/>
-                </Card> 
-            </div>
-            <div className="col-lg-6 col-md-6">
-                <Card  >
-                    <Card.Body>
-                        <Card.Title>{product.title}</Card.Title>
-                        <Card.Text>
-                            {product.description}
-                        </Card.Text>
-                    </Card.Body>
-                    <ListGroup className="list-group-flush">
-                        <ListGroup.Item>Precio: ${product.price} - Cantidad: {product.cant}   
-                            <button className="btn btn-danger" onClick={() => removeProduct(product.id)}>X</button>
-                        </ListGroup.Item>
-                    </ListGroup>
-                </Card> 
-            </div>
-            </div>
-            )}
-            <p>Precio total = ${totalPrice()}</p>
-            <button className="btn btn-outline-danger" onClick={emptyCart}>Vaciar Carrito</button>
-            <button className="btn btn-outline-dark" onClick={getOrder}>Realizar pedido</button>
-            </div>
-        </div>
+        <>
+            {isId !== '' && <h2>El Id de la compra es {isId} </h2>}
+            { cartList.length === 0 ? 
+                <>
+                    <h2 className="text-center">No hay productos en el carrito</h2>
+                </>
+                :
+                <div>
+                    <div className="container"> 
+                    {cartList.map(product => 
+                    <div className="row">
+                    <div className="col-lg-3 col-md-6" >
+                        <Card>
+                            <Card.Img variant="top" src={product.img} alt={product.title} className="rounded d-block"/>
+                        </Card> 
+                    </div>
+                    <div className="col-lg-6 col-md-6">
+                        <Card  >
+                            <Card.Body>
+                                <Card.Title>{product.title}</Card.Title>
+                                <Card.Text>
+                                    {product.description}
+                                </Card.Text>
+                            </Card.Body>
+                            <ListGroup className="list-group-flush">
+                                <ListGroup.Item>Precio: ${product.price} - Cantidad: {product.cant}   
+                                    <button className="btn btn-danger" onClick={() => removeProduct(product.id)}>X</button>
+                                </ListGroup.Item>
+                            </ListGroup>
+                        </Card> 
+                    </div>
+                    </div>
+                    )}
+                    {totalPrice() !== 0 && <p className="text-center">Precio total = ${totalPrice()}</p>}
+                    </div>
+                    <form className="text-center" onSubmit={getOrder}>
+                        <label>Ingresar nombre</label><br />
+                        <input type="text" name="name" required onChange={handleOnChange} value={formData.name}/><br />
+                        <label>Ingresar Telefono</label><br />
+                        <input type="text" name="phone" required onChange={handleOnChange} value={formData.phone}/><br />
+                        <label>Ingresar Email</label><br />
+                        <input type="text" name="email" required onChange={handleOnChange} value={formData.email}/><br />
+                        <label>Ingresar Direccion</label><br />
+                        <input type="text" name="adress" required onChange={handleOnChange} value={formData.adress}/><br />
+                        <button className="btn btn-outline-dark" onClick={getOrder}>Realizar pedido</button>
+                        <button className="btn btn-outline-danger" onClick={emptyCart}>Vaciar Carrito</button>
+                    </form>        
+                </div>
+            }
+        </>
     )
 }
